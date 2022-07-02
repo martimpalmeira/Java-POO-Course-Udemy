f936a3a0d8b761e6187b6b3b41493b39ac91500e
martimpalmeira <martim.desenvolvedor@gmail.com>
2022-07-02 17:01:54 -0300

    practice 160

--- Poo_Udemy/src/exercises/secao15/practice160/exception/DateException.java
+++ Poo_Udemy/src/exercises/secao15/practice160/exception/DateException.java
@@ -1,6 +1,12 @@
 package exercises.secao15.practice160.exception;
 
-public class DateException {
+public class DateException extends Exception {
 	
+	private static final long serialVersionUID = 1L;
+	
+	
+	public DateException (String msg) {
+		super(msg);
+	}
 	
 }
